a591d5d89cece199986c9f6f182c1e96ed973913
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-04 16:48:14 -0300

    update welcome route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 const UserController = require('../Controllers/UserController');
 
 routes.get('/', async(req, res) => {
-    res.send({ 'message': 'Bem vindo a API do iPet'})
+    res.send({ 'message': 'Bem vindo a API do Ruptiva Code Challenge'})
 });
 
 routes.get('/user', UserController.index)
